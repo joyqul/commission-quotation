2329a5bd2204c6efa987ff9fafa50996f3e2aa83
joyqul <joyqul.tw@gmail.com>
2020-09-16 21:15:37 +0800

    Updates

---
+++ precache-manifest.41e2c2a081a08b0aaf1a6b9796efa2dc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9c975f31f863aba5dafa183c298fdb2",
+    "url": "/commission-quotation/index.html"
+  },
+  {
+    "revision": "36ecdc78f3244da3be9a",
+    "url": "/commission-quotation/static/css/2.1c164e0d.chunk.css"
+  },
+  {
+    "revision": "9283967682f33a00480b",
+    "url": "/commission-quotation/static/css/main.c7e2b943.chunk.css"
+  },
+  {
+    "revision": "36ecdc78f3244da3be9a",
+    "url": "/commission-quotation/static/js/2.c9e823a0.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/commission-quotation/static/js/2.c9e823a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9283967682f33a00480b",
+    "url": "/commission-quotation/static/js/main.8871f885.chunk.js"
+  },
+  {
+    "revision": "4e10c869257c49836e5a",
+    "url": "/commission-quotation/static/js/runtime-main.6d2e66e5.js"
+  }
+]);
