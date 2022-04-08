4346550f7c5ba0b237167a1bec5405011b2569e3
joyqul <joyqul@users.noreply.github.com>
2022-04-08 08:53:49 +0000

    deploy: a5fb9ff4e5b6b944ed87631eeeb2a0af8d44d064

---
+++ precache-manifest.dc83e992a847ba0286413aa32ad13a60.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "010c45f37d667c7c738c97c8db075767",
+    "url": "/commission-quotation/index.html"
+  },
+  {
+    "revision": "a6451e993e26bbf10163",
+    "url": "/commission-quotation/static/css/main.b32f7eb9.chunk.css"
+  },
+  {
+    "revision": "385d21ea8b3621b9591b",
+    "url": "/commission-quotation/static/js/2.0f8b9642.chunk.js"
+  },
+  {
+    "revision": "b8700c277388707df0cbe192bbd6ca86",
+    "url": "/commission-quotation/static/js/2.0f8b9642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6451e993e26bbf10163",
+    "url": "/commission-quotation/static/js/main.8ec648a1.chunk.js"
+  },
+  {
+    "revision": "4e10c869257c49836e5a",
+    "url": "/commission-quotation/static/js/runtime-main.6d2e66e5.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/commission-quotation/precache-manifest.c7a065467797990eb99ecc955269e349.js"
+  "/commission-quotation/precache-manifest.dc83e992a847ba0286413aa32ad13a60.js"
 );
 
 self.addEventListener('message', (event) => {
